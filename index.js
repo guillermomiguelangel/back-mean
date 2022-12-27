73af47e98a514842027af55cfa0a9f10a652b964
Guimian <guillermomiguelangel83@gmail.com>
2022-12-27 19:43:28 -0300

    add ui

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ app.use( express.json() );
 app.use( '/api/auth', require('./routes/auth'));
 
 //manejar otras rutas
-app.use( '*', (req,res)=>{
-  res.sendFile(path.resolve(__dirname, './public/index.html'))
+app.get( '*', (req,res)=>{
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
 });
 
 app.listen( port , () => {
